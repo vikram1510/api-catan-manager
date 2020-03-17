643b9859d977bca344bb0e7f467689bd1f5c19f1
bagejar <rahul.bageja@ig.com>
2020-03-17 19:37:55 +0000

    changed port number

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser')
 
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/catan'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3030
 
 const players = require('./controllers/players')
 
