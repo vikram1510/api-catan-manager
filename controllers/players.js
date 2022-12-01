abcb8cdc9774cbb0aa2620dbcd10efd8dad2470d
Vikram Bageja <vikram.bageja@gmail.com>
2022-12-01 22:52:05 +0000

    Do some logging

--- controllers/players.js
+++ controllers/players.js
@@ -1,67 +1,70 @@
-const Player = require('../models/Player')
+const Player = require("../models/Player");
 
-function create(req, res){
+function create(req, res) {
   Player.create(req.body)
-    .then(player => res.json(player))
-    .catch(err => res.status(400).json(err))
-} 
+    .then((player) => res.json(player))
+    .catch((err) => res.status(400).json(err));
+}
 
-function index(req, res){
-  const { name } =  req.query
-  const findPlayer = name ? Player.find({ name }) : Player.find()
-  
+function index(req, res) {
+  console.log("/players", req);
+  const { name } = req.query;
+  const findPlayer = name ? Player.find({ name }) : Player.find();
+  console.log("player from db", findPlayer);
+
   findPlayer
-    .then(players => res.json(players))
-    .catch(err => res.status(400).json(err))
+    .then((players) => {
+      console.log("Found players", players);
+      return res.json(players);
+    })
+    .catch((err) => res.status(400).json(err));
 }
 
-function update(req, res){
-  Player
-    .findById(req.params.id)
-    .then(player => player.set(req.body))
-    .then(player => player.save())
-    .then(player => res.json(player))
-    .catch(err => res.status(400).json(err))
+function update(req, res) {
+  Player.findById(req.params.id)
+    .then((player) => player.set(req.body))
+    .then((player) => player.save())
+    .then((player) => res.json(player))
+    .catch((err) => res.status(400).json(err));
 }
 
-function remove(req, res){
-  Player
-    .findById(req.params.id)
-    .then(player => player.remove())
+function remove(req, res) {
+  Player.findById(req.params.id)
+    .then((player) => player.remove())
     .then(() => res.sendStatus(204))
-    .catch(err => res.status(400).json(err))
+    .catch((err) => res.status(400).json(err));
 }
 
 function show(req, res) {
   Player.findById(req.params.id)
-    .then(player => res.json(player))
-    .catch(err => res.status(404).json(err))
+    .then((player) => res.json(player))
+    .catch((err) => res.status(404).json(err));
 }
 
-async function transaction(req, res){
-
-  const { amounts, fromId, toId } = req.body
+async function transaction(req, res) {
+  const { amounts, fromId, toId } = req.body;
 
   const decAmounts = Object.keys(amounts).reduce((final, resource) => {
-    final[resource] = amounts[resource] * -1
-    return final
-  }, {})
+    final[resource] = amounts[resource] * -1;
+    return final;
+  }, {});
 
-  await Player.findOneAndUpdate({ _id: fromId },{ $inc: { ...decAmounts } } )
-  await Player.findOneAndUpdate({ _id: toId },{ $inc: { ...amounts } } )
-  
-  return res.json({ message: 'Transaction successfull' })
+  await Player.findOneAndUpdate({ _id: fromId }, { $inc: { ...decAmounts } });
+  await Player.findOneAndUpdate({ _id: toId }, { $inc: { ...amounts } });
 
+  return res.json({ message: "Transaction successfull" });
 }
 
-async function bank(req, res){
+async function bank(req, res) {
+  const { playerId, amounts } = req.body;
 
-  const { playerId, amounts } = req.body
+  const updatedPlayer = await Player.findOneAndUpdate(
+    { _id: playerId },
+    { $inc: { ...amounts } },
+    { new: true }
+  );
 
-  const updatedPlayer = await Player.findOneAndUpdate({ _id: playerId },{ $inc: { ...amounts } }, { new: true } )
-
-  res.json(updatedPlayer)
-
+  res.json(updatedPlayer);
 }
 
-module.exports = { create, index, update, remove, show, transaction, bank }
+module.exports = { create, index, update, remove, show, transaction, bank };
